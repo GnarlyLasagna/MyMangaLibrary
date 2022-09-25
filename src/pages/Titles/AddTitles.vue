9d0396d344e8bf9dc2bbe51dc63925ba5a0a6145
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2022-09-25 00:18:11 -0500

    Add files via upload

--- src/pages/Titles/AddTitles.vue
+++ src/pages/Titles/AddTitles.vue
@@ -1,4 +1,17 @@
 <template>
-  <section>FILTER</section>
-  <section>AddTitles</section>
+  <section>
+    <base-card>
+      <h2>add title</h2>
+      <ItemForm></ItemForm>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import ItemForm from '../../components/titles/TitleForm.vue';
+export default {
+  components: {
+    ItemForm,
+  },
+};
+</script>
